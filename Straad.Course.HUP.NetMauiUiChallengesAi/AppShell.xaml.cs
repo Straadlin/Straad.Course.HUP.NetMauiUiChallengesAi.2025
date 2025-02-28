6ae1945d99e2f77639a29bb330b2ef6807c3d1f6
Alfredo Estrada <Straadlin@users.noreply.github.com>
2025-02-28 02:25:53 -0600

    The project base was created.

---
+++ Straad.Course.HUP.NetMauiUiChallengesAi/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Straad.Course.HUP.NetMauiUiChallengesAi
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
